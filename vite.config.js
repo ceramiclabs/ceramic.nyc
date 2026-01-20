27378d994ad242c53e6080020de981c232b2eb1f
Philip Forget <philipforget@gmail.com>
2026-01-20 14:19:17 -0500

    actions for staging

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path"
 
 export default defineConfig({
   root: "./src",
+  base: process.env.VITE_BASE_PATH || "/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
