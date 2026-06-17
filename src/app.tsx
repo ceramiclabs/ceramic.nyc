58f8ea753ac4b7a69f7c18be248c5b9841b93d16
Brian Tobin <brianjtobin@gmail.com>
2026-06-17 13:23:38 -0400

    Quick update

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import { Header } from "./components/header"
 import { Footer } from "./components/footer"
 import { SectionNav } from "./components/section-nav"
 import { ChevronButton } from "./components/chevron-button"
-import { AlsideCaseStudy } from "./components/alside-case-study"
 import { ArenaCaseStudy } from "./components/arena-case-study"
 import { RendevuCaseStudy } from "./components/rendevu-case-study"
 import {
@@ -20,7 +19,6 @@ export function App() {
       <main>
         <IntroSection />
         <ProductSection />
-        <AlsideCaseStudy />
         <ArenaCaseStudy />
         <RendevuCaseStudy />
         <ContactSection />
